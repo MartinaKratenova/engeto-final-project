65c2e86b24e675d370177e389bf1ba7bdc70c1b5
Martina Kratenova <martina.tokova@gmail.com>
2026-09-22 16:38:22 +0200

    pool added

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import express from 'express';
 
+import pkg from 'pg';
+
+const { Pool } = pkg;
 const app = express();
 const port = 3000;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.render('index', {
     cssName: '/index.css',
     jsName: '/index.js'
   });
 
 });
+
+
 
 
 
