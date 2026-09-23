61a9bc542976f5e2a192d53b580f66b57b593737
Martina Kratenova <martina.tokova@gmail.com>
2026-09-23 16:09:22 +0200

    admin page basic structure, list, pg formatting

--- index.js
+++ index.js
@@ -28,18 +28,34 @@ app.get('/', (req, res, next) => {
 
 
 app.get('/dishes', async (req, res, next) => {
- 
-    const result = await pool.query('SELECT * FROM menu_items');
 
-    res.render('dishes', {
-      cssName: '/css/dishes.css',
-      jsName: '/js/dishes.js',
-      dishes: result.rows
+  const result = await pool.query('SELECT * FROM menu_items');
 
-    });
+  res.render('dishes', {
+    cssName: '/css/dishes.css',
+    jsName: '/js/dishes.js',
+    dishes: result.rows
+
+  });
 
 
 });
+
+app.get('/admin', async (req, res, next) => {
+
+  const result = await pool.query('SELECT m.name, m.description, m.price, c.name as category, c.id, m.id from menu_items as m left join categories as c ON c.id = m.category_id');
+
+  res.render('admin', {
+    cssName: '/css/admin.css',
+    jsName: '/js/admin.js',
+    dishes: result.rows
+   
+  });
+
+ console.log(result.rows)
+});
+
+
 
 
 
